232122b889e4545a7d494c49358b685c8f086f77
klazapp <klazapp@klazapp.com>
2023-12-31 17:20:41 +0800

    Add files via upload

---
+++ Editor/Data/HierarchyColorComponent.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace com.Klazapp.Editor
+{
+    public static class HierarchyColorComponent
+    {
+        public static readonly Color32 nonEmptyBackgroundColor = new Color32(52, 140, 117, 255);
+        public static readonly Color32 emptyBackgroundColor = new Color32(75, 75, 75, 255);
+        public static readonly Color32 emptyTextColor = new Color32(255, 255, 255, 255);
+
+        public static readonly Color32 todoBackgroundColor = new Color32(155, 55, 75, 255);
+
+        public static readonly Color32 InEditorBackgroundColor = new Color32(115, 115, 115, 255);
+    }
+}
